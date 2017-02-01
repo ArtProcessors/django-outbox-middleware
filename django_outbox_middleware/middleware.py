b36ae49fe89b34ee0c207e19874ef826d15d17b7
Jacob Haslehurst <jacob@haslehurst.net>
2017-02-01 11:47:41 +1100

    add request path and (optional) body logging

--- django_outbox_middleware/middleware.py
+++ django_outbox_middleware/middleware.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.http import HttpResponse
 
 try:
@@ -19,9 +20,16 @@ class OutboxMiddleware(MiddlewareMixin):
 
     def process_response(self, request, response):
         if 'HTTP_OUTBOX_REQUEST_UUID' in request.META:
+            if getattr(settings, 'OUTBOX_LOG_BODY', False):
+                request_body = request.body
+            else:
+                request_body = None
+
             OutboxRequestLog.objects.create(
                 request_uuid=request.META['HTTP_OUTBOX_REQUEST_UUID'],
                 response_status_code=response.status_code,
+                request_body=request_body,
+                request_path=request.get_full_path(),
             )
 
         return response
