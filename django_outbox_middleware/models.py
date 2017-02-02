b0a5b51f3adb7af81c2e015f1e6f9a44b1ad27d1
Jacob Haslehurst <jacob@haslehurst.net>
2017-02-02 15:50:36 +1100

    add index to request_uuid field

--- django_outbox_middleware/models.py
+++ django_outbox_middleware/models.py
@@ -6,7 +6,7 @@ from model_utils.models import TimeStampedModel
 
 
 class OutboxRequestLog(TimeStampedModel):
-    request_uuid = models.UUIDField()
+    request_uuid = models.UUIDField(db_index=True)
     request_path = models.TextField()
     request_body = models.TextField(null=True, blank=True)
     request_flagged_duplicate = models.BooleanField()
